415a779eee041add31c4828ed0616f1b81d76380
spacemonkey1101 <ritambh54@gmail.com>
2022-11-29 22:49:16 +0530

    initializing array to remove garbage

--- 1_array_input_output_update.cpp
+++ 1_array_input_output_update.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main()
 {
-    int marks[100], n; // reserving a array for 100 students
+    // int marks[100], n; // reserving a array for 100 students
+    int marks[100] = {0}, n; // initializing with 0
+    // will set all values as 0, removing the garbage
 
     cout << "Enter the number of marks for students" << endl;
     cin >> n;
@@ -22,10 +24,10 @@ int main()
 
     cout << "The marks are " << endl;
 
-    for (int i = 0; i <= n + 1; i++)
+    for (int i = 0; i < 100; i++)
     {
         cout << marks[i] << ",";
     }
     cout << endl;
-    cout << "we will see that the value at position " << n  << " is garbage" << endl;
+    cout << "we will see that the value are not filled with garbage but is initialized with the value" << endl;
 }
