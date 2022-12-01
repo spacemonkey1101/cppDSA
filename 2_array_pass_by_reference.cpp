839389029f8a0a417b189866cdf87716f6941eb0
spacemonkey1101 <ritambh54@gmail.com>
2022-12-01 10:48:53 +0530

    passing array reference and trying to print it

--- 2_array_pass_by_reference.cpp
+++ 2_array_pass_by_reference.cpp
@@ -5,7 +5,16 @@ using namespace std;
 void printArray(int arr[])
 {
     // pointer variable has a size of 4 bytes
-    cout << "In printArray function " << sizeof(arr) << endl;
+    // cout << "In printArray function " << sizeof(arr) << endl;
+
+    int size = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ,";
+    }
+    
+    //the above code prints
+    //1 ,
 }
 
 int main()
@@ -15,6 +24,16 @@ int main()
 
     // here the pointer is aware that its pointing to an array
     // hence it is aware of its size = 24bytes (6*4)
-    cout << "In main function " << sizeof(arr) << endl;
+    // cout << "In main function " << sizeof(arr) << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ,";
+    }
+    //the above code prints
+    //1 ,2 ,3 ,3 ,4 ,5 ,
+    cout << endl;
+
+    //only an address is passed 
     printArray(arr);
 }
