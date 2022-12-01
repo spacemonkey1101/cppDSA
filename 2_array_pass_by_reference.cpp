a54ea21f645334774437fd24cb5f3a9bff38efb3
spacemonkey1101 <ritambh54@gmail.com>
2022-12-01 10:50:09 +0530

    passing array reference and its size

--- 2_array_pass_by_reference.cpp
+++ 2_array_pass_by_reference.cpp
@@ -2,19 +2,18 @@
 
 using namespace std;
 
-void printArray(int arr[])
+void printArray(int arr[], int size)
 {
     // pointer variable has a size of 4 bytes
     // cout << "In printArray function " << sizeof(arr) << endl;
 
-    int size = sizeof(arr) / sizeof(int);
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ,";
     }
     
     //the above code prints
-    //1 ,
+    //1 ,2 ,3 ,3 ,4 ,5 ,
 }
 
 int main()
@@ -34,6 +33,6 @@ int main()
     //1 ,2 ,3 ,3 ,4 ,5 ,
     cout << endl;
 
-    //only an address is passed 
-    printArray(arr);
+    // address and size is passed 
+    printArray(arr,size);
 }
