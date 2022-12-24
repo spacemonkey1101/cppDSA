a909c74ae9a60baf739b4b4d5fa654f8ab1a1bdb
spacemonkey1101 <ritambh54@gmail.com>
2022-12-24 10:40:09 +0530

    array of string

---
+++ 31_array_of_strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char s_array[][100] = {
+        "cry",
+        "every",
+        "last",
+        "tear"};
+
+    // this will print the first string
+    cout << s_array[0];
+}
