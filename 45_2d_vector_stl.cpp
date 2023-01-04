34dd6f444c5543e78121ef1c9e803e1e6f4230db
spacemonkey1101 <ritambh54@gmail.com>
2023-01-04 10:49:07 +0530

    2d vector

---
+++ 45_2d_vector_stl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> arr = {
+        {1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9}};
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int number : arr[i])
+        {
+            cout << number << " ";
+        }
+        cout << endl;
+    }
+}
