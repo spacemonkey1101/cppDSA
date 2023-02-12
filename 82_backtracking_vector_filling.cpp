433c451cfaf6f9d3fcd1c3d4b0ca6da5558459e9
spacemonkey1101 <ritambh54@gmail.com>
2023-02-12 19:41:47 +0530

    backtracking with regular vectors

---
+++ 82_backtracking_vector_filling.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printArray(vector<int> v, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+void fillArray(vector<int> v, int i, int n)
+{
+    // base case
+    if (i == n)
+    {
+        printArray(v, n);
+        return;
+    }
+    v[i] = i + 1;
+    // recursive case
+    fillArray(v, i + 1, n);
+    // backtracking step
+    v[i] = v[i] * -1;
+}
+
+int main()
+{
+    // initializing all elements to 0
+    vector<int> v(100,0);
+    int n;
+    cout << "Enter the value of n " << endl;
+    cin >> n;
+
+    fillArray(v, 0, n);
+    printArray(v, n);
+
+    return 0;
+}
