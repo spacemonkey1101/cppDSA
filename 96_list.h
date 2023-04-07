17af6a4f66e29b9abcb05e3796111126130ffa7c
spacemonkey1101 <ritambh54@gmail.com>
2023-04-07 23:35:14 +0530

    List ADT and Node definition

---
+++ 96_list.h
@@ -0,0 +1,28 @@
+class Node
+{
+    int data;
+    Node *next;
+
+public:
+    /*
+    We can create the constructor as a initialization list
+    Node(int data):this->data(data),next(NULL){}
+    */
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+}
+
+class List
+{
+    Node *head; // start of the list
+    Node *tail; // not needed but it stores the end of the lis
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+};
