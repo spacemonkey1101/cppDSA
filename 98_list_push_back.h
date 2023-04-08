e8cbee4654384ac979a04370174d35b1b007928c
spacemonkey1101 <ritambh54@gmail.com>
2023-04-08 09:56:48 +0530

    linked list | push back function

---
+++ 98_list_push_back.h
@@ -0,0 +1,53 @@
+class Node
+{
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int data)
+    {
+        if (head == NULL)
+        {
+            Node *n = new Node(data);
+            head = tail = n;
+        }
+        else
+        {
+            Node *n = new Node(data);
+            n->next = head;
+            head = n;
+        }
+    }
+
+    void push_back(int data)
+    {
+        if (head == NULL)
+        {
+            Node *n = new Node(data);
+            head = tail = n;
+        }
+        else
+        {
+            Node *n = new Node(data);
+            tail->next = n;
+            tail = n;
+        }
+    }
+};
