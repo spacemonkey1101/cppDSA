0fcf340e2e9a16a5824d897ef2bc3b9fdfab0f18
spacemonkey1101 <ritambh54@gmail.com>
2023-04-29 22:03:38 +0530

    delete at a position in linked  list

--- 97_linked_list.cpp
+++ 97_linked_list.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "106_linked_list_pop_back.h"
+#include "107_delete_position_linked_list.h"
 
 using namespace std;
 
@@ -17,6 +17,14 @@ int main()
     l.insert(10, 0);
     l.print_list();
     // 10 0 1 2 3
+
+    cout << l.search(10) << endl;  // 0
+    cout << l.search(2) << endl;   // 3
+    cout << l.search(101) << endl; //-1
+
+    cout << l.recursiveSearch(101) << endl; //-1
+    cout << l.recursiveSearch(2) << endl;   // 3
+    cout << l.recursiveSearch(10) << endl;  // 0
 
     l.pop_front();
     l.print_list();
@@ -41,13 +49,18 @@ int main()
     l.pop_back();
     l.pop_front();
 
-    /*cout << l.search(10) << endl;  // 0
-    cout << l.search(2) << endl;   // 3
-    cout << l.search(101) << endl; //-1
+    l.push_front(1);
+    l.push_front(0);
+    l.push_back(3);
+    l.print_list();
+    // 0 1 3
 
-    cout << l.recursiveSearch(101) << endl; //-1
-    cout << l.recursiveSearch(2) << endl;   // 3
-    cout << l.recursiveSearch(10) << endl;  // 0
-    */
+    l.delete_position(1);
+    l.print_list();
+    // 0 3
+    l.delete_position(0);
+    l.print_list();
+    // 3
+
     return 0;
 }
