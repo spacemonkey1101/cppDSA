e502790cb5394e128434ea7a3166f39e04dd99f2
spacemonkey1101 <ritambh54@gmail.com>
2023-05-08 00:29:30 +0530

    next greater element | stack optimization

--- 117_next_greater_element.cpp
+++ 117_next_greater_element.cpp
@@ -23,3 +23,23 @@ using namespace std;
 // }/
 
 //Optimize with Stack
+vector<int> nextGreaterElement(vector<int> v){
+    vector<int> res;
+    stack<int> s,holder;
+    for (int i = v.size()-1; i>=0; i--){
+        while(!s.empty() && v[i]>=s.top()){
+            s.pop();
+        }
+        if(s.empty())
+            holder.push(-1);
+        else 
+            holder.push(s.top());
+            
+        s.push(v[i]);
+    }
+    while(!holder.empty()){
+        res.push_back(holder.top());
+        holder.pop();
+    }
+    return res;
+}
