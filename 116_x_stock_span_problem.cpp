8e0bfb6d699c0e7405df6459829ad1bdfd1b0727
spacemonkey1101 <ritambh54@gmail.com>
2023-06-01 20:37:57 +0530

    stock span using stack

--- 116_x_stock_span_problem.cpp
+++ 116_x_stock_span_problem.cpp
@@ -1,5 +1,7 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <utility>
 
 using namespace std;
 
@@ -17,7 +19,7 @@ vector<int> stockSpannerBruteForce(vector<int> &stocks)
                 break;
             }
         }
-        if (j== i-1)
+        if (j == i - 1)
         {
             res.push_back(1);
         }
@@ -28,14 +30,60 @@ vector<int> stockSpannerBruteForce(vector<int> &stocks)
     }
     return res;
 }
+
+vector<int> stockSpannerImproved(vector<int> &stocks)
+{
+    stack<pair<int, int>> s;
+    int size = stocks.size();
+    vector<int> res;
+    int i, j;
+
+    for (i = 0; i < size; i++)
+    {
+        int counter = 1;
+
+        while (!s.empty())
+        {
+            pair<int, int> p, t = s.top();
+            if (stocks[i] < t.first)
+            {
+                p.first = stocks[i];
+                p.second = counter;
+                s.push(p);
+                res.push_back(p.second);
+                break;
+            }
+            else
+            {
+                s.pop();
+                counter = counter + t.second;
+            }
+        }
+
+        if (s.empty())
+        {
+            pair<int, int> p(stocks[i], 1);
+            s.push(p);
+            res.push_back(p.second);
+        }
+    }
+    return res;
+}
+
 int main()
 {
     vector<int> stocks = {100, 80, 60, 70, 60, 75, 85};
-    vector<int> resBrute = stockSpanner(stocks);
+    vector<int> resBrute = stockSpannerBruteForce(stocks);
+    vector<int> resImproved = stockSpannerImproved(stocks);
 
     for (int i = 0; i < resBrute.size(); i++)
     {
         cout << resBrute[i] << ", ";
     }
-    cout<<endl;
+    cout << endl;
+
+    for (int i = 0; i < resImproved.size(); i++)
+    {
+        cout << resImproved[i] << ", ";
+    }
 }
