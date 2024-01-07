d034d3f85ffd9cc310dedac145e1019112f5c3b3
spacemonkey1101 <ritambh54@gmail.com>
2024-01-07 21:33:34 +0530

    binary tree level order print

---
+++ 153.binary_tree_level_order_print.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node *preorder_build()
+{
+    int d;
+    cin >> d;
+
+    if (d == -1)
+    {
+        return NULL;
+    }
+
+    Node *n = new Node(d);
+    n->left = preorder_build();
+    n->right = preorder_build();
+
+    return n;
+}
+
+void level_order_traversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty() && !(q.size() == 1 && q.front() == NULL) )
+    {
+        if (q.front() != NULL)
+        {
+            Node *t = q.front();
+            cout << t->data << " ";
+            if (t->left != NULL)
+            {
+                q.push(t->left);
+            }
+            if (t->right != NULL)
+            {
+                q.push(t->right);
+            }
+        }
+        else
+        {
+            cout << endl;
+            q.push(NULL);
+        }
+        q.pop();
+    }
+}
+
+int main()
+{
+    Node *t = preorder_build();
+    // INPUT : 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+    level_order_traversal(t);
+    // Expected OUTPUT : 1 2 4 5 7 3 6
+
+    return 0;
+}
